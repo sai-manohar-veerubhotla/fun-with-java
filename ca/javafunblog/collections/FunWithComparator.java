c5bf6ce3979ab13e011c17e237161e226f9804c7
javaguy <manoharvs@outlook.com>
2022-10-10 20:58:45 -0400

    fun on day 14

---
+++ ca/javafunblog/collections/FunWithComparator.java
@@ -0,0 +1,48 @@
+package ca.javafunblog.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunWithComparator {
+    private static void fun() {
+        Person person1 = new Person(26, "Vamsi");
+        Person person2 = new Person(25, "Manohar");
+        Person person3 = new Person(2, "alexa");
+
+        List<Person> people = new ArrayList<>();
+        people.add(person1);
+        people.add(person2);
+        people.add(person3);
+
+        System.out.println("people: " + people);
+        Collections.sort(people,
+                (p1, p2) -> Integer.valueOf(p1.getAge()).compareTo(p2.getAge()));
+
+        System.out.println("people sorted by age: " + people);
+
+        Collections.sort(people,
+                (p1, p2) -> p1.getName().compareTo(p2.getName()));
+
+        System.out.println("people sorted by name: " + people);
+
+
+        // SAME AS ABOVE!!!!
+        Collections.sort(people,
+                Comparator.comparing(Person::getName));
+
+
+
+        // Function -> takes one input -> gives one output
+        // Supplier -> takes nothing -> but gives output
+        // Consumer -> takes something -> gives nothing
+        // Predicate -> takes something -> tells if its false or true
+
+    }
+
+    public static void main(String[] args) {
+        fun();
+    }
+}
---
+++ ca/javafunblog/collections/ListUtil.java
@@ -0,0 +1,42 @@
+package ca.javafunblog.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class ListUtil {
+
+
+    public static List<Integer> listNovice(int size, int bound) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(bound));
+        }
+
+        return list;
+    }
+
+    public static List<Integer> list(int size, int bound) {
+        Random random = new Random();
+        // METHOD CHAINING
+        return random.ints(size, 1, bound)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+//        randomDemo();
+        System.out.println(listNovice(4, 100));
+        System.out.println(list(4, 100));
+    }
+
+
+    private static void randomDemo() {
+        Random random = new Random();
+        System.out.println(random.nextInt());
+        System.out.println(random.nextInt(100));
+    }
+
+}
