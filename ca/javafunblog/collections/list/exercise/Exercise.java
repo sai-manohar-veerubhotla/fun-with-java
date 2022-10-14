451179bcf15ed1edbf904ea0853af3081948d9bb
javaguy <manoharvs@outlook.com>
2022-10-13 20:52:27 -0400

    fun on day 16

---
+++ ca/javafunblog/collections/list/exercise/Exercise.java
@@ -0,0 +1,21 @@
+package ca.javafunblog.collections.list.exercise;
+
+public class Exercise {
+
+
+    // take a big string with comma separated values
+    // "manohar, vamsi , iPhone, Alexa"
+    // split it into words
+    // collect the words into a list
+    // use streams
+
+    // take a string -> split into characters
+    // "Manohar" -> ['M', 'a', ...]
+    // use filter and get me only vowels
+    // a, e, i, o, u
+
+
+
+
+
+}
